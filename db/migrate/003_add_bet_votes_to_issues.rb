37bb797a6d60e5c0d4449f24571b869c7f55f037
Edmagne Santos <edmagne@yahoo.com.br>
2012-01-25 17:29:30 -0200

    corrections

--- db/migrate/003_add_bet_votes_to_issues.rb
+++ db/migrate/003_add_bet_votes_to_issues.rb
@@ -1,6 +1,7 @@
 class AddBetVotesToIssues < ActiveRecord::Migration
   def self.up
     add_column :issues, :bet_votes, :integer, :default => 0
+    Issue.update_all(:bet_votes => 0)
   end
 
   def self.down
